fa9fa163f233fb3d22f5d819011614b11785930f
samzhangjy <samzhang951@outlook.com>
2021-10-10 16:52:11 +0800

    :sparkles: added a `404` page

---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container text-center">
+    <h1>404</h1>
+    <hr />
+    <br />
+    The page you are looking for is lost in the fields.
+    <div class="center">
+      <vs-button size="large" @click="$router.push('/')">Go back home</vs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  mounted: function () {
+    this.changeTitle('404')
+  }
+}
+</script>
